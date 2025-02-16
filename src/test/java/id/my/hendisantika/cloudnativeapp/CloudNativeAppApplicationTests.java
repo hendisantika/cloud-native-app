275c15c03d8dd217a8bf0cc7f01737fcb09ac7e6
hendisantika <hendisantika@yahoo.co.id>
2025-02-16 16:15:51 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳⬆

---
+++ src/test/java/id/my/hendisantika/cloudnativeapp/CloudNativeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.cloudnativeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNativeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
